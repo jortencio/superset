6f2638fca57ec36b5356aa7611302b8c977e1c60
Jason Ortencio <jason.ortencio@puppet.com>
2021-10-12 17:59:56 +1100

    Uninstall python packes

--- manifests/python.pp
+++ manifests/python.pp
@@ -9,15 +9,15 @@ class superset::python {
   $python_pips = lookup('superset::python_pips')
   $python_venvs = lookup('superset::python_pyvenvs')
 
-  class { 'python' :
-    ensure   => 'absent',
-    version  => lookup('superset::python_version', String),
-    pip      => 'absent',
-    dev      => 'absent',
-    gunicorn => 'absent',
+#  class { 'python' :
+#    ensure   => 'absent',
+#    version  => lookup('superset::python_version', String),
+#    pip      => 'absent',
+#    dev      => 'absent',
+#    gunicorn => 'absent',
 #    python_pips    => $python_pips,
 #    python_pyvenvs => $python_venvs,
-  }
+#  }
 
   $package_uninstall = ['python3-wheel','python3-pip','python3-devel']
 
