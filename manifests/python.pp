54880333a9465bd7b4d344ad8a9a882a41a9ba86
Jason Ortencio <jason.ortencio@puppet.com>
2021-10-12 11:39:28 +1100

    Fixed up python class and added pythonmodule dependences to fixture

--- manifests/python.pp
+++ manifests/python.pp
@@ -6,28 +6,10 @@
 #   include superset::python
 class superset::python {
   class { 'python' :
-    ensure     => 'present',
-    version    => 'system',
-    dev        => 'present',
-    virtualenv => 'present'
-  }
-
-  $virtualenv_path = lookup('superset::venv_path', String)
-
-  python::pip { 'apache-superset':
-    ensure     => 'present',
-    pkgname    => 'apache-superset',
-    virtualenv => $virtualenv_path
-  }
-
-  $superset_user ='superset'
-
-  python::virtualenv { $virtualenv_path :
-    ensure   => present,
+    ensure   => 'present',
     version  => 'system',
-    venv_dir => "/home/${superset_user}/virtualenvs",
-    owner    => $superset_user,
-    group    => $superset_user,
-    cwd      => $virtualenv_path,
+    pip      => 'present',
+    dev      => 'present',
+    gunicorn => 'present'
   }
 }
