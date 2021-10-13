14f1763df7af1e7218141845d97a70aa486d4a38
Jason Ortencio <jason.ortencio@puppet.com>
2021-10-13 15:35:03 +1100

    Fixed exec path

--- manifests/install.pp
+++ manifests/install.pp
@@ -15,7 +15,7 @@ class superset::install {
   exec {'Initialize DB':
     command  => 'superset db upgrade',
     creates  => '/root/.superset/superset.db',
-    path     => $venv_dir,
+    path     => "${venv_dir}/bin",
     provider => 'shell',
     require  => Class['superset::python']
   }
@@ -24,7 +24,7 @@ class superset::install {
   exec { 'Create Admin User':
     command  => 'superset fab create-admin --username admin --firstname admin --lastname --admin --password password --email jason.ortencio@puppet.com',
     unless   => 'superset fab list-users | grep admin',
-    path     => $venv_dir,
+    path     => "${venv_dir}/bin",
     provider => 'shell',
     require  => [Class['superset::python'],Exec['Initialize DB']]
   }
