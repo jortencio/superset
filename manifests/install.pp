d02ed04e7fc5934de0301af495da1cccac86b54e
Jason Ortencio <jason.ortencio@puppet.com>
2021-10-13 13:49:08 +1100

    Add steps to initialise a SQLlite database and create an admin user

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,4 +6,20 @@
 #   include superset::install
 class superset::install {
   include superset::python
+
+  # Set up for development instance only
+
+  $venv_dir = lookup('superset::virtual_env_dir', String)
+
+  # Use of SQLite will be deprecated at some point
+  exec {'Initialize DB':
+    command => ". ${venv_dir}/bin/activate; superset db upgrade",
+    creates => '/root/.superset/superset.db'
+  }
+
+  # Add parameters here
+  exec { 'Create Admin User':
+    command => ". ${venv_dir}/bin/activate; superset fab create-admin --username admin --firstname admin --lastname --admin --password password --email jason.ortencio@puppet.com",
+    unless  => ". ${venv_dir}/bin/activate; superset fab list-users | grep admin"
+  }
 }
