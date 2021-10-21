bbe145e5072a73316d24307bd36258371f4b3789
Jason Ortencio <jason.ortencio@puppet.com>
2021-10-21 16:36:58 +1100

    Remove custom fact for now

--- lib/facter/superset_version.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Facter.add(:superset_version) do
-  confine kernel: 'Linux'
-  setcode do
-    '/home/superset/apache-superset/bin/superset version | grep Superset'
-  end
-end
