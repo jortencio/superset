fd97c25698fb6b3f7cc496097e952d4ebb11d6b1
Jason Ortencio <jason.ortencio@puppet.com>
2021-10-21 17:51:14 +1100

    Added test coverage report and added tests for some resources

--- spec/classes/superset_spec.rb
+++ spec/classes/superset_spec.rb
@@ -8,6 +8,16 @@ describe 'superset' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+      it { is_expected.to contain_class('superset::packages') }
+      it { is_expected.to contain_class('superset::python') }
+      it { is_expected.to contain_class('superset::install') }
+      it { is_expected.to contain_class('superset::config') }
+      it { is_expected.to contain_class('superset::postgresql') }
+      it { is_expected.to contain_class('superset::init_db') }
+      it { is_expected.to contain_class('superset::service') }
+
+      #ToDo add conditional test for firewalld
+
     end
   end
 end
