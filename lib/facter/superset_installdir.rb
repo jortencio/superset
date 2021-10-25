8ec75db88d005f12eaed5a8f142abb5d5e9dd480
Jason Ortencio <jason.ortencio@puppet.com>
2021-10-25 16:16:32 +1100

    Fix linting issues

--- lib/facter/superset_installdir.rb
+++ lib/facter/superset_installdir.rb
@@ -4,6 +4,6 @@ Facter.add(:superset_installdir) do
   # https://puppet.com/docs/puppet/latest/fact_overview.html
   setcode do
     path = Facter::Core::Execution.execute('echo $SUPERSET_CONFIG_PATH')
-    path.sub('/superset_config.py','')
+    path.sub('/superset_config.py', '')
   end
 end
