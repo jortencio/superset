a723ce4f6deec2080f20632f1aca548490194d0e
Jason Ortencio <jason.ortencio@puppet.com>
2021-10-25 16:31:28 +1100

    Fixed superset_version to run superset based on path from superset_installdir

--- lib/facter/superset_version.rb
+++ lib/facter/superset_version.rb
@@ -5,7 +5,8 @@ Facter.add(:superset_version) do
   # https://puppet.com/docs/puppet/latest/fact_overview.html
 
   setcode do
-    superset_version = Facter::Core::Execution.execute('/home/superset/apache-superset/bin/superset version | grep Superset | sed -r "s/\\x1B\\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"',
+    install_dir = Facter.value(:superset_installdir)
+    superset_version = Facter::Core::Execution.execute("#{install_dir}/bin/superset version | grep Superset | sed -r \"s/\\x1B\\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g\"",
 { on_fail: 'Superset Version Not Found' })
     superset_version.split(' ')[1]
   end
