fa20793e5b8b3c3dba82feb0f91735412d98bf8d
Jason Ortencio <jason.ortencio@puppet.com>
2021-10-26 16:33:37 +1100

    Changed implementation for getting config for database uri

--- lib/facter/superset_config.rb
+++ lib/facter/superset_config.rb
@@ -11,8 +11,15 @@ Facter.add(:superset_config) do
     config_file.each_line do |line|
       # Skip comments and empty lines
       next if line.start_with?('#') || line.empty?
-      if line.include? ' = '
-        data = line.split(' = ')
+      next unless line.include? ' = '
+      data = line.split(' = ')
+      if data[0] == 'SQLALCHEMY_DATABASE_URI'
+        config['SUPERSET_DATABASE'] = {}
+        config['SUPERSET_DATABASE']['TYPE'] = data[1].chomp.match(%r{((?<=').+(?=://))})[0]
+        config['SUPERSET_DATABASE']['HOSTNAME'] = data[1].chomp.match(%r{((?<=@).+(?=:\d+|:\d/))})[0]
+        config['SUPERSET_DATABASE']['PORT'] = data[1].chomp.match(%r{((?<=:)\d+(?=/))})[0]
+        config['SUPERSET_DATABASE']['DB_NAME'] = data[1].chomp.match(%r{((?<=\d/).+$)})[0]
+      else
         config[data[0]] = data[1].chomp
       end
     end
