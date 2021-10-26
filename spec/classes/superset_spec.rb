a7460930317a0f9e853222dc4c7c2db4fffe0dfd
Jason Ortencio <jason.ortencio@puppet.com>
2021-10-26 15:06:47 +1100

    Cleaned up rspec tests to have seperate scenarios

--- spec/classes/superset_spec.rb
+++ spec/classes/superset_spec.rb
@@ -7,9 +7,24 @@ describe 'superset', :class do
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to compile }
+      context 'With no parameters set'
+      it { is_expected.to compile.and_raise_error(%r{expects a value for parameter 'admin_config'}) }
 
-      context 'With default values' do 
+      context 'With default values and the admin_config set' do
+        let(:params) do
+          {
+            admin_config: {
+              username: 'admin',
+              password: 'password',
+              firstname: 'admin',
+              lastname: 'admin',
+              email: 'admin@mycompany.com'
+            }
+          }
+        end
+
+        it { is_expected.to compile }
+
         it { is_expected.to contain_class('superset::packages') }
 
         if os_facts[:operatingsystem] == 'RedHat' # Need to find out how to check for opersatingsystemrelease
@@ -17,7 +32,60 @@ describe 'superset', :class do
         end
 
         packages.each do |package|
-          it { is_expected.to contain_package(package) }
+          it { is_expected.to contain_package(package).with_ensure('installed') }
+        end
+
+        it { is_expected.to contain_class('superset::python') }
+
+        it { is_expected.to contain_class('superset::install') }
+        it { is_expected.to contain_user('superset') }
+        it { is_expected.to contain_group('superset') }
+        it { is_expected.to contain_file('/etc/profile.d/superset.sh') }
+        it { is_expected.to contain_Python__Pyvenv('/home/superset/apache-superset') }
+        it { is_expected.to contain_Python__Pip('apache-superset') }
+        it { is_expected.to contain_Python__Pip('gevent') }
+        it { is_expected.to contain_Python__Pip('gunicorn') }
+        it { is_expected.to contain_Python__Pip('psycopg2') }
+
+        it { is_expected.to contain_class('superset::config') }
+
+        it { is_expected.to contain_class('superset::postgresql') }
+        it { is_expected.to contain_Postgresql__Server__Db('superset') }
+
+        it { is_expected.to contain_class('superset::init_db') }
+        it { is_expected.to contain_exec('Create Admin User') }
+        it { is_expected.to contain_exec('Initialize DB') }
+        it { is_expected.to contain_exec('Initialize default roles and permissions') }
+
+        it { is_expected.to contain_class('superset::service') }
+        it { is_expected.to contain_file('/bin/superset.gunicorn') }
+        it { is_expected.to contain_file('/usr/lib/systemd/system/superset.service') }
+        it { is_expected.to contain_service('superset') }
+
+        it { is_expected.to contain_file('/home/superset/apache-superset/superset_config.py') }
+      end
+
+      context 'With manage_python set to true and the admin_config set' do
+        let(:params) do
+          { 'manage_python': true, admin_config: {
+            username: 'admin',
+          password: 'password',
+          firstname: 'admin',
+          lastname: 'admin',
+          email: 'admin@mycompany.com'
+          } }
+        end
+
+        it { is_expected.to compile }
+
+        it { is_expected.to contain_class('superset::packages') }
+
+        if os_facts[:operatingsystem] == 'RedHat' # Need to find out how to check for opersatingsystemrelease
+          packages = ['gcc', 'gcc-c++', 'libffi-devel', 'cyrus-sasl-devel', 'openssl-devel', 'openldap-devel', 'postgresql-devel']
+        end
+
+        packages.each do |package|
+          it { is_expected.to contain_package(package).with_ensure('installed') }
         end
 
         it { is_expected.to contain_class('superset::python') }
@@ -49,11 +117,6 @@ describe 'superset', :class do
 
         it { is_expected.to contain_file('/home/superset/apache-superset/superset_config.py') }
 
-      end
-
-      context 'With manage_python set to true' do
-        let(:params) { { 'manage_python': true } }
-
         it { is_expected.to contain_class('Python') }
 
         if os_facts[:operatingsystem] == 'RedHat'
@@ -62,8 +125,57 @@ describe 'superset', :class do
         end
       end
 
-      context 'With manage_python set to true' do
-        let(:params) { { 'manage_firewall': true } }
+      context 'With manage_python set to true and the admin_config set' do
+        let(:params) do
+          { 'manage_firewall': true, admin_config: {
+            username: 'admin',
+        password: 'password',
+        firstname: 'admin',
+        lastname: 'admin',
+        email: 'admin@mycompany.com'
+          } }
+        end
+
+        it { is_expected.to compile }
+
+        it { is_expected.to contain_class('superset::packages') }
+
+        if os_facts[:operatingsystem] == 'RedHat' # Need to find out how to check for opersatingsystemrelease
+          packages = ['gcc', 'gcc-c++', 'libffi-devel', 'cyrus-sasl-devel', 'openssl-devel', 'openldap-devel', 'postgresql-devel']
+        end
+
+        packages.each do |package|
+          it { is_expected.to contain_package(package).with_ensure('installed') }
+        end
+
+        it { is_expected.to contain_class('superset::python') }
+
+        it { is_expected.to contain_class('superset::install') }
+        it { is_expected.to contain_user('superset') }
+        it { is_expected.to contain_group('superset') }
+        it { is_expected.to contain_file('/etc/profile.d/superset.sh') }
+        it { is_expected.to contain_Python__Pyvenv('/home/superset/apache-superset') }
+        it { is_expected.to contain_Python__Pip('apache-superset') }
+        it { is_expected.to contain_Python__Pip('gevent') }
+        it { is_expected.to contain_Python__Pip('gunicorn') }
+        it { is_expected.to contain_Python__Pip('psycopg2') }
+
+        it { is_expected.to contain_class('superset::config') }
+
+        it { is_expected.to contain_class('superset::postgresql') }
+        it { is_expected.to contain_Postgresql__Server__Db('superset') }
+
+        it { is_expected.to contain_class('superset::init_db') }
+        it { is_expected.to contain_exec('Create Admin User') }
+        it { is_expected.to contain_exec('Initialize DB') }
+        it { is_expected.to contain_exec('Initialize default roles and permissions') }
+
+        it { is_expected.to contain_class('superset::service') }
+        it { is_expected.to contain_file('/bin/superset.gunicorn') }
+        it { is_expected.to contain_file('/usr/lib/systemd/system/superset.service') }
+        it { is_expected.to contain_service('superset') }
+
+        it { is_expected.to contain_file('/home/superset/apache-superset/superset_config.py') }
 
         it { is_expected.to contain_class('superset::firewalld') }
         it { is_expected.to contain_Firewalld_port('Open the port used for superset: 8088') }
