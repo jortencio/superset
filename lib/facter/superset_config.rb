444490cba62a30ae91f5728fc616d7b9ce6d009a
Jason Ortencio <jason.ortencio@puppet.com>
2021-10-26 14:56:47 +1100

    Cleaned up linting issues in superset_config

--- lib/facter/superset_config.rb
+++ lib/facter/superset_config.rb
@@ -6,14 +6,14 @@ Facter.add(:superset_config) do
     config = {}
     install_dir = Facter.value(:superset_installdir)
 
-    config_file = File.open("#{install_dir}/superset_config.py", "r")
+    config_file = File.open("#{install_dir}/superset_config.py", 'r')
 
     config_file.each_line do |line|
-      if line.start_with? '#' || line.empty?
-        next
-      elsif line.include? ' = '
+      # Skip comments and empty lines
+      next if line.start_with?('#') || line.empty?
+      if line.include? ' = '
         data = line.split(' = ')
-        config[data[0]] = data[1].chomp 
+        config[data[0]] = data[1].chomp
       end
     end
     config
--- spec/unit/facter/superset_config_spec.rb
+++ spec/unit/facter/superset_config_spec.rb
@@ -6,6 +6,7 @@ require 'facter/superset_config'
 
 describe :superset_config, type: :fact do
   subject(:fact) { Facter.fact(:superset_config) }
+
   let(:content) { StringIO.new("#\nkey1 = value1\n\n#\nkey2 = value2\n") }
 
   before :each do
@@ -13,10 +14,10 @@ describe :superset_config, type: :fact do
     Facter.clear
     allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
     allow(Facter).to receive(:value).with(:superset_installdir).and_return('/home/superset/apache-superset')
-    allow(File).to receive(:open).with("/home/superset/apache-superset/superset_config.py", "r").and_return(content)
+    allow(File).to receive(:open).with('/home/superset/apache-superset/superset_config.py', 'r').and_return(content)
   end
 
   it 'returns a value' do
-    expect(fact.value).to eq({ "key1" => 'value1', "key2" => 'value2'})
+    expect(fact.value).to eq({ 'key1' => 'value1', 'key2' => 'value2' })
   end
 end
