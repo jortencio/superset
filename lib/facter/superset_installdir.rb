39065dea9c167dc112b541e6f003bcdda62a39b3
Jason Ortencio <jason.ortencio@puppet.com>
2021-12-22 13:23:16 +1100

    trying again to execute echo

--- lib/facter/superset_installdir.rb
+++ lib/facter/superset_installdir.rb
@@ -4,6 +4,6 @@ Facter.add(:superset_installdir) do
   confine kernel: 'Linux'
   setcode do
     path = Facter::Core::Execution.execute('/usr/bin/echo $SUPERSET_CONFIG_PATH')
-    path.sub('/superset_config.py', '')
+    path = path.sub('/superset_config.py', '')
   end
 end
