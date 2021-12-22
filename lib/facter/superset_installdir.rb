b7a9b8bec8a43e14ccc48ab5c2113b5f65e61a0e
Jason Ortencio <jason.ortencio@puppet.com>
2021-12-22 16:14:15 +1100

    Replaced superset_installdir custom fact with external fact

--- lib/facter/superset_installdir.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Facter.add(:superset_installdir) do
-  confine kernel: 'Linux'
-  setcode do
-    Facter::Core::Execution.execute('/bin/bash -c "env"')
-    # path.sub('/superset_config.py', '')
-  end
-end
