7d1ed3da8246d160605b71e93b0771dee46bdff7
Jason Ortencio <jason.ortencio@puppet.com>
2021-12-22 15:16:42 +1100

    Testing bash command exec in custom fact

--- lib/facter/superset_installdir.rb
+++ lib/facter/superset_installdir.rb
@@ -3,7 +3,7 @@
 Facter.add(:superset_installdir) do
   confine kernel: 'Linux'
   setcode do
-    Facter::Core::Execution.execute('env')
+    Facter::Core::Execution.execute('/bin/bash -c "env"')
     # path.sub('/superset_config.py', '')
   end
 end
