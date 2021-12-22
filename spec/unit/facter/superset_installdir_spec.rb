d9299e725995537d5c7ad9925dbf5c6ae253021f
Jason Ortencio <jason.ortencio@puppet.com>
2021-12-22 12:56:25 +1100

    Test out other environment variable to see if it prints

--- spec/unit/facter/superset_installdir_spec.rb
+++ spec/unit/facter/superset_installdir_spec.rb
@@ -11,7 +11,7 @@ describe :superset_installdir, type: :fact do
     # perform any action that should be run before every test
     Facter.clear
     allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
-    allow(Facter::Core::Execution).to receive(:execute).with('echo $SUPERSET_CONFIG_PATH').and_return('/opt/apache-superset/superset_config.py')
+    allow(Facter::Core::Execution).to receive(:execute).with('echo $USER').and_return('/opt/apache-superset/superset_config.py')
   end
 
   it 'returns a value' do
