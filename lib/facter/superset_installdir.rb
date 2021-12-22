07555771694cd92d411dfcb1e0541bebbadafd63
Jason Ortencio <jason.ortencio@puppet.com>
2021-12-22 13:28:05 +1100

    Try basic echo

--- lib/facter/superset_installdir.rb
+++ lib/facter/superset_installdir.rb
@@ -3,7 +3,7 @@
 Facter.add(:superset_installdir) do
   confine kernel: 'Linux'
   setcode do
-    path = Facter::Core::Execution.execute('/usr/bin/echo $SUPERSET_CONFIG_PATH')
-    path = path.sub('/superset_config.py', '')
+    Facter::Core::Execution.execute('/usr/bin/echo test')
+    # path.sub('/superset_config.py', '')
   end
 end
--- spec/unit/facter/superset_installdir_spec.rb
+++ spec/unit/facter/superset_installdir_spec.rb
@@ -11,10 +11,10 @@ describe :superset_installdir, type: :fact do
     # perform any action that should be run before every test
     Facter.clear
     allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
-    allow(Facter::Core::Execution).to receive(:execute).with('/usr/bin/echo $SUPERSET_CONFIG_PATH').and_return('/opt/apache-superset/superset_config.py')
+    allow(Facter::Core::Execution).to receive(:execute).with('/usr/bin/echo test').and_return('/opt/apache-superset/superset_config.py')
   end
 
   it 'returns a value' do
-    expect(fact.value).to eq('/opt/apache-superset')
+    expect(fact.value).to eq('/opt/apache-superset/superset_config.py')
   end
 end
