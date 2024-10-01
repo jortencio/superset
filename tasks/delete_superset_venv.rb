ace40a25dd12f8069f38985b892c4fe5bf8a0c0a
Jason Ortencio <jason.ortencio@puppet.com>
2024-10-01 11:02:25 +1000

    Replace implementation of removal of directory

--- tasks/delete_superset_venv.rb
+++ tasks/delete_superset_venv.rb
@@ -1,12 +1,15 @@
 #! /opt/puppetlabs/puppet/bin/ruby
 # A task for deleting the Apache Superset virtual environment directory
 require 'facter'
+require 'fireutils'
 
 superset_install_dir = Facter.value('superset_installdir')
 
-begin
-  Dir.rmdir(superset_install_dir)
+if Dir.exists? (superset_install_dir)
+  FileUtils.rm_rf(superset_install_dir, :secure=>true)
   puts "Apache Superset virtual environment directory '#{superset_install_dir}' deleted successfully!"
-rescue SystemCallError => e
-  puts "Apache Superset virtual environment directory deletion failed: #{e.message}"
+  exit 0
+else
+  puts "Apache Superset virtual environment directory '#{e.message}' does not exist"
+  exit 1
 end
